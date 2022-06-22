63ce3d1841c8991823ef6c5effa89f2847f2ed07
simon wilson <simonw101@gmail.com>
2022-06-22 12:45:50 +0100

    added initial screen and detail movie controller

---
+++ Movie Information/MovieDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MovieDetailViewController.swift
+//  Movie Information
+//
+//  Created by Simon Wilson on 22/06/2022.
+//
+
+import UIKit
+
+class MovieDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
