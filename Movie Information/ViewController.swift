dea473b84790e81da6ab6275d2a3fdd07a96cbde
simon wilson <simonw101@gmail.com>
2022-06-22 12:15:54 +0100

    Initial Commit

---
+++ Movie Information/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie Information
+//
+//  Created by Simon Wilson on 22/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
